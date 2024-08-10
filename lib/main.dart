1bd56ad3d23c41d9f193ec05209b38ba388df87f
ymk74 <swimmer.shark12@gmail.com>
2024-08-11 00:41:24 +0300

    create Dio method to test the request and response

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:news_app/views/home_view.dart';
 
 void main() {
+  getNews();
   runApp(const NewsApp());
 }
 
+final dio = Dio();
+
+void getNews() async {
+  final response = await dio.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=4cb3201f20c8455999a1220db93cfd5f');
+  print(response);
+}
 class NewsApp extends StatelessWidget {
   const NewsApp({super.key});
 
