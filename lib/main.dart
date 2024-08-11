7ee2ccdb2db4d8f4ad176ba44d6efe5d28cf01ab
ymk74 <swimmer.shark12@gmail.com>
2024-08-11 17:37:36 +0300

    create list to get articles

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,13 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:news_app/services/news_service.dart';
 import 'package:news_app/views/home_view.dart';
 
 void main() {
-  getNews();
+  NewsService(Dio()).getNews();
   runApp(const NewsApp());
 }
 
-final dio = Dio();
-
-void getNews() async {
-  final response = await dio.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=4cb3201f20c8455999a1220db93cfd5f');
-  print(response);
-}
 class NewsApp extends StatelessWidget {
   const NewsApp({super.key});
 
--- lib/services/news_service.dart
+++ lib/services/news_service.dart
@@ -9,5 +9,10 @@ class NewsService {
     Response response = await dio.get(
       'https://newsapi.org/v2/top-headlines?apiKey=4cb3201f20c8455999a1220db93cfd5f&country=eg&category=general',
     );
+    Map<String, dynamic> jsonData = response.data;
+    List<dynamic> articles = jsonData['articles'];
+    // List<Map<String, dynamic>> articles =
+    //     jsonData['articles'] as List<Map<String, dynamic>>;
+    print(articles);
   }
 }
