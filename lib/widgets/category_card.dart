1f8a7d02ed3b5901625ccbd809e5c8f4208a0dfa
ymk74 <swimmer.shark12@gmail.com>
2024-08-13 17:14:45 +0300

    Create category view and Navication to it

--- lib/widgets/category_card.dart
+++ lib/widgets/category_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/models/category_model.dart';
+import 'package:news_app/views/category_view.dart';
 
 class CategoryCard extends StatelessWidget {
   const CategoryCard({super.key, required this.category});
@@ -7,25 +8,36 @@ class CategoryCard extends StatelessWidget {
   final CategoryModel category;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(right: 10),
-      child: Container(
-        height: 85,
-        width: 160,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(12),
-          image: DecorationImage(
-            image: AssetImage(category.image),
-            fit: BoxFit.cover,
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) {
+              return CategoryView();
+            },
           ),
-        ),
-        child: Center(
-          child: Text(
-            category.categoryName,
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: 16,
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.only(right: 10),
+        child: Container(
+          height: 85,
+          width: 160,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            image: DecorationImage(
+              image: AssetImage(category.image),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Center(
+            child: Text(
+              category.categoryName,
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
             ),
           ),
         ),
