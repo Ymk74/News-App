bb7c41875ddd3c2060201d41ba7b3c2400639c06
ymk74 <swimmer.shark12@gmail.com>
2024-08-13 16:16:50 +0300

    Refactor Ui in news list view builder by future builder

--- lib/widgets/news_list_view_builder.dart
+++ lib/widgets/news_list_view_builder.dart
@@ -1,39 +1,29 @@
-
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:news_app/models/article_model.dart';
 import 'package:news_app/services/news_service.dart';
 import 'package:news_app/widgets/news_list_view.dart';
 
-class NewsListViewBuilder extends StatefulWidget {
-  const NewsListViewBuilder({
-    super.key,
-  });
-  @override
-  State<NewsListViewBuilder> createState() => _NewsListViewBuilderState();
-}
-class _NewsListViewBuilderState extends State<NewsListViewBuilder> {
-  List<ArticleModel> articles = [];
-  bool isLoading = true;
-  @override
-  void initState() {
-    super.initState();
-    getGeneralNews();
-  }
-  Future<void> getGeneralNews() async {
-    articles = await NewsService(Dio()).getNews();
-    isLoading = false;
-    setState(() {});
-  }
+class NewsListViewBuilder extends StatelessWidget {
+  const NewsListViewBuilder({super.key});
   @override
   Widget build(BuildContext context) {
-    return isLoading
-        ? const SliverToBoxAdapter(
-            child: Center(
-            child: CircularProgressIndicator(),
-          ))
-        : articles.isNotEmpty ? NewsListView(
-            articles: articles,
-          ) : const SliverToBoxAdapter(child: Center(child: Text('Oops there was an error, try again later')));
+    return FutureBuilder(
+        future: NewsService(Dio()).getNews(),
+        builder: (context, snapshot) {
+          return NewsListView(
+            articles: snapshot.data ?? [],
+          );
+        });
+
+    //   return isLoading
+    //       ? const SliverToBoxAdapter(
+    //           child: Center(
+    //           child: CircularProgressIndicator(),
+    //         ))
+    //       : articles.isNotEmpty ? NewsListView(
+    //           articles: articles,
+    //         ) : const SliverToBoxAdapter(child: Center(child: Text('Oops there was an error, try again later')));
+    // }
   }
 }
